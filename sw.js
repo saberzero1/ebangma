a6521eb4d7ec9957c11e078f09f8eb7cc760ce07
saberzero1 <ewjbangma@hotmail.com>
2021-12-06 11:24:50 +0100

    Updated service worker

--- sw.js
+++ sw.js
@@ -1,12 +1,9 @@
 var APP_PREFIX = 'EmileBangmaPortfolio_';     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_02';                  // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_03';                  // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION;
 var URLS = [                                // Add URL you want to cache in this list.                                  // If you have separate JS/CSS files,
-  '/index.html',                            // add path to those files here
-  '/style/',
-  '/icons/',
-  '/media/',
-  '/fonts/'
+  './index.html',                            // add path to those files here
+  './style/style.css'
 ];
 
 // Respond with cached resources
