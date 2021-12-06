52ec970994f18658f2da3c85ed0d0da5df4707dc
saberzero1 <ewjbangma@hotmail.com>
2021-12-06 10:55:03 +0100

    Updated service worker

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
+var APP_PREFIX = 'EmileBangmaPortfolio_'     // Identifier for this app (this needs to be consistent across every cache update)
 var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
-  '/{repository}/',                     // If you have separate JS/CSS files,
-  '/{repository}/index.html'            // add path to those files here
+  '/',                                  // If you have separate JS/CSS files,
+  '/index.html',                        // add path to those files here
+  '/style/',
+  '/icons/'
 ]
 
 // Respond with cached resources
