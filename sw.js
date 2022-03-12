fd829661cadc0c91ac2e159d9a19d310ce0821d1
saberzero1 <ewjbangma@hotmail.com>
2022-03-12 23:42:09 +0100

    Updated comments

--- sw.js
+++ sw.js
@@ -12,7 +12,6 @@ var URLS = [                                  // Add URL you want to cache in th
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url);
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (!request) // if cache is available, respond with cache
